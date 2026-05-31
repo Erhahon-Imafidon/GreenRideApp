87e1a677da1047a6cef3654c10bb7ddb5244a54a
Erhahon Imafidon <mafiosco4@gmail.com>
2026-05-31 05:28:47 +0100

    refactor: fix the safeareaview imports error on multiple screens

--- src/screens/ConfirmRideScreen.tsx
+++ src/screens/ConfirmRideScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, SafeAreaView, ScrollView } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { useAppDispatch, useAppSelector } from '../hooks/reduxHook';
