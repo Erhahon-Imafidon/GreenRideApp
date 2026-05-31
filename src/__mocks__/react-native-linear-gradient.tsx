e994b47db73f39231c0c44768729d6fca9be5627
Erhahon Imafidon <mafiosco4@gmail.com>
2026-05-31 05:01:40 +0100

    feat: add GradientButton and ScreenHeader components with linear gradient styling

---
+++ src/__mocks__/react-native-linear-gradient.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { View } from 'react-native';
+const LinearGradient = ({ children, ...props }: any) => <View {...props}>{children}</View>;
+export default LinearGradient;
