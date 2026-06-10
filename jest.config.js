8e5e614084023981fa12140aafff391b31231c25
Erhahon Imafidon <mafiosco4@gmail.com>
2026-06-10 10:27:05 +0100

    fix: update jest.config.js to include react-native-permissions and adjust transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,10 @@ module.exports = {
     moduleNameMapper: {
         'react-native-linear-gradient':
             '<rootDir>/src/__mocks__/react-native-linear-gradient.tsx',
+        'react-native-permissions':
+            '<rootDir>/src/__mocks__/react-native-permissions.ts',
     },
     transformIgnorePatterns: [
-        'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@mgcrea/react-native-tailwind|@reduxjs/toolkit|immer|redux)/)',
+        'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@mgcrea/react-native-tailwind|@reduxjs/toolkit|immer|redux|@react-navigation)/)',
     ],
 };
